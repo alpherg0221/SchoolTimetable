8646efc53d75d3a739f57bf0ab99d2c882efe6dc
alpherg0221 <alpherg0221@gmail.com>
2021-12-10 14:41:19 +0900

    Add splash screen

--- app/src/main/java/jp/gr/java_conf/alpherg0221/schooltimetable/ui/MainActivity.kt
+++ app/src/main/java/jp/gr/java_conf/alpherg0221/schooltimetable/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package jp.gr.java_conf.alpherg0221.schooltimetable.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import jp.gr.java_conf.alpherg0221.schooltimetable.SchoolTimetableApplication
 
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        val splashScreen = installSplashScreen()
 
         val appContainer = (application as SchoolTimetableApplication).container
 
